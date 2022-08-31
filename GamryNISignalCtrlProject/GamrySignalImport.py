cfebd73320695440360f3ef09ae6be4ec572cf34
Fiavi Y <50966363+Vast-Horizon@users.noreply.github.com>
2022-08-31 11:45:28 -0400

    Update Gamry project code

--- GamryNISignalCtrlProject/GamrySignalImport.py
+++ GamryNISignalCtrlProject/GamrySignalImport.py
@@ -1,7 +1,8 @@
 __author__ = "Fiavi(Zhaoen) Yang"
 #Github: https://github.com/Vast-Horizon/Gamry-Signal-Generator-Python3
 
-from PyQt6 import QtWidgets, uic,QtCore, QtGui
+from PyQt6 import QtWidgets, uic,QtCore
+from PyQt6.QtGui import QDoubleValidator
 from tkinter import filedialog
 from pyqtgraph import PlotWidget
 import comtypes
@@ -9,6 +10,7 @@ import comtypes.client as client
 import gc
 import time
 from datetime import datetime
+import numpy as np
 import os
 import sys
 import csv
@@ -16,7 +18,7 @@ import csv
 active = False #Flag
 global mode
 mode = "Gstat" #default mode is galvanostat
-fpath = 'InputProfile_Default.csv' #default name of the signal data file
+fpath = 'RevProfile_Copy.csv' #default path of the signal data file
 
 ############################Gamry Classes#################################
 class GamryCOMError(Exception):
@@ -28,7 +30,7 @@ def gamry_error_decoder(e):
         if hresult & 0x20000000:
             return GamryCOMError('0x{0:08x}: {1}'.format(2**32+e.args[0], e.args[1]))
     return e
-c = 0
+
 class GamryDtaqEvents(object):
     def __init__(self, dtaq):
         self.dtaq = dtaq
@@ -55,10 +57,17 @@ class UI(QtWidgets.QMainWindow):
    #Load UI 
     def __init__(self):
         super(UI, self).__init__()
-        uic.loadUi("MainProjectUI.ui", self)
+        uic.loadUi("GamryProjectUI.ui", self)
         self.graphicsView.setBackground('w')
         self.graphicsView_2.setBackground('w')
         self.graphicsView_3.setBackground('w')
+        # FrequencyInput and ampInput only allow float input
+        FreqValidator = QDoubleValidator()
+        ampValidator = QDoubleValidator()
+        FreqValidator.setRange(0,200,2)
+        ampValidator.setRange(0,100,4)
+        self.FrequencyInput.setValidator(FreqValidator)
+        self.ampInput.setValidator(ampValidator)
         #Get current time
         currentDay = datetime.now().day
         currentMonth = datetime.now().month
@@ -67,7 +76,7 @@ class UI(QtWidgets.QMainWindow):
         self.progressBar.setValue(0)   
         #Check if the default signal file exist
         if os.path.isfile(fpath) == False:
-            print("Note: The default signal file"+fpath+"does not appear to exist in current directory.\n Please select an input file.") 
+            print("The default signal file"+fpath+"does not appear to exist in current directory. Please select an input file.") 
             self.signalPathlabel.setText("Please select an input signal data file")
         else:
             self.signalPathlabel.setText(fpath)
@@ -88,12 +97,9 @@ class UI(QtWidgets.QMainWindow):
     #Creat Directory to save
     def folderDir(self):
         objname = self.objNameEdit.text()
-        temperature = self.TempEdit.text()
-        testnum = self.TestNumEdit.text()
-        dday = str(self.dateEdit.date().day())
-        mmonth = str(self.dateEdit.date().month())
-        yyyyear = str(self.dateEdit.date().year())
-        subdir = "Test"+testnum+"-"+temperature + "-"+mmonth+"-"+dday+"-"+yyyyear
+        condition = self.conditionEdit.text()
+        Temp = self.TempEdit.text()
+        subdir = "Temp"+Temp+" "+condition
         cdir = os.getcwd() #Get the current directory
         parent_dir = os.path.join(cdir,objname)
         global outputPath
@@ -122,7 +128,10 @@ class UI(QtWidgets.QMainWindow):
         devices=client.CreateObject('GamryCOM.GamryDeviceList')
         #Gamry Instrument object
         pstat=client.CreateObject('GamryCOM.GamryPC6Pstat')
-        pstat.Init(devices.EnumSections()[0]) 
+        try:
+            pstat.Init(devices.EnumSections()[0])
+        except IndexError:
+            raise Exception("\n**ERROR - Unable to initialize. Restart the instrument then try again**\n")
         #Open
         pstat.Open()
         pstat.SetCtrlMode(GamryCOM.PstatMode)#Set it to Potentiostat mode
@@ -144,7 +153,10 @@ class UI(QtWidgets.QMainWindow):
         devices=client.CreateObject('GamryCOM.GamryDeviceList')
         #Gamry Instrument object
         pstat=client.CreateObject('GamryCOM.GamryPC6Pstat')
-        pstat.Init(devices.EnumSections()[0]) 
+        try:
+            pstat.Init(devices.EnumSections()[0])
+        except IndexError:
+            raise Exception("\n**ERROR - Unable to initialize. Restart the instrument then try again**\n")
         #Open
         pstat.Open()
         pstat.SetCtrlMode(GamryCOM.GstatMode)#Set it to galvanostat mode
@@ -161,30 +173,39 @@ class UI(QtWidgets.QMainWindow):
     def toclose(self):
             pstat.SetCell(GamryCOM.CellOff)
             pstat.Close()
+            self.IndicatorLabel.setStyleSheet("QLabel {background-color: rgb(50,200,50);border: 1.5px inset gray;border-radius: 8px;}")
             gc.collect()
+            
 
     #To test    
     def test(self):
         #Prepare parameters
         amp = float(self.ampInput.text())
-        global numOfPoints, PointsList
+        global numOfPoints, PointsList,SampleRate
         try:
             f = open(fpath)
             self.IndicatorLabel.setStyleSheet("QLabel {background-color: rgb(255,224,102);border: 1.5px inset gray;border-radius: 8px;}")
             PointsList = f.readlines()
             numOfPoints = len(PointsList)
             PointsList = [float(i)*amp for i in PointsList]
-        except (NameError, IOError): #If error, stop test()
+        except (NameError, IOError):#If error, stop test()
             print("Error - No Input Signal file Is Selected or the file does not appear to exist.") 
             self.signalPathlabel.setText("Error - No Signal Profile Is Selected")
             self.toclose()
             return False
-
+        global timeList
         Cycles = int(self.spinBox.text())
-        temprate = float(numOfPoints/(int(self.FrequencyInput.text())*1000)/numOfPoints)
+        temprate = float(1/((float(self.FrequencyInput.text())*1000)))
         temprate = round(temprate,8)
         SampleRate = temprate
-        Sig=client.CreateObject('GamryCOM.GamrySignalArray')
+        timeList = []
+        timeVal = 0
+        for i in range(numOfPoints):
+            timeList.append(timeVal) 
+            timeList[i] = timeVal
+            timeVal+=SampleRate
+
+        Sig=client.CreateObject('GamryCOM.GamrySignalArray')#Create Signal Object
         if mode == "Gstat":
             Sig.Init(pstat, Cycles, SampleRate, numOfPoints, PointsList, GamryCOM.GstatMode)
         elif mode == "Pstat":
@@ -193,10 +214,10 @@ class UI(QtWidgets.QMainWindow):
 
         #Print out information
         print("###################################################")
-        print("Number of Data Points of each period: ", numOfPoints)
+        print("Number of Data Points in each cycle: ", numOfPoints)
         print("Number of Cycles: ", Cycles)
         print("Time gap between each output data point in second: ", SampleRate)
-        runTime = SampleRate*Cycles*numOfPoints
+        runTime = SampleRate*Cycles*(numOfPoints-1)
         print("Estimated total signal length in second: ", runTime)
         print("Frequcency of data point outputing in Hz: ", 1/SampleRate) 
         print("###################################################")
@@ -210,6 +231,7 @@ class UI(QtWidgets.QMainWindow):
                 dtaqcpiv.Run(True)
             print("Running...")
         except Exception as e:
+            #self.toclose()
             raise gamry_error_decoder(e)
         prograssList = []
         counter = 0
@@ -225,7 +247,7 @@ class UI(QtWidgets.QMainWindow):
  
         #Turn off
         while active == False: 
-            self.progressBar.setValue(30)  
+            self.progressBar.setValue(30) 
             self.clear()
             self.draw()
             print("Terminating...")
@@ -234,7 +256,6 @@ class UI(QtWidgets.QMainWindow):
             try:
                 f.close()
                 break
-
             except (NameError, IOError):
                 pass
         self.progressBar.setValue(0)
@@ -242,42 +263,47 @@ class UI(QtWidgets.QMainWindow):
         #Save Raw Data to the directory
         self.folderDir()
         testnum = self.TestNumEdit.text()
+        temp = self.TempEdit.text()
         cellID = self.IDEdit.text()
-        model = self.objNameEdit.text()
-        subdir = outputPath+"\Test"+testnum +"-Object"+cellID+"-"+model+".csv"
+        condition = self.conditionEdit.text()
+        dday = str(self.dateEdit.date().day())
+        mmonth = str(self.dateEdit.date().month())
+        yyear = str(self.dateEdit.date().year())
+        yyear = yyear[-2:]
+        datafileout = outputPath+"\Test"+testnum +"ID"+cellID+"Temp"+temp+condition+mmonth+dday+yyear+".csv"
+        #subdir = outputPath+"\S"+cellID +"T"+testnum+posit+"T"+temp+"290822.csv"
         rawDataList = []
-        #titleList = ["Time(s)","Input","Measur. V(V)","Uncompens. V","Measur. I(A)","Vsig","Aux Input","IE Range","Overload Info","Stop","Temp"]
-        titleList = ["Time(s)","Input","Measur. V(V)","Measur. I(A)"]
+        titleList = ["Time[s]","Sampling Time[s]","Input Signal","Measur. V[V]","Measur. I[A]"]
         c=0
-        with open(subdir, 'w') as file_handler:
-            writer = csv.writer(file_handler)
-            writer.writerow(titleList)
+        with open(datafileout, 'w') as file_handler:
+            #writer = csv.writer(file_handler)
+            # for row in r:
+            #     writer.writerow(row)
+            #===========================================================
             #convert a list of tuples to a list of strings, note: 
                 #dtaqsink.acquired_points is a list of tuples
                 #item is a list of string lists
                 #rawDataList is a list of strings, and stritem is string
+            #===========================================================
             for item in dtaqsink.acquired_points:
-                item = [item[i] for i in (0,1,3)] #delete or comment out this line to save all data
-                item.insert(1,PointsList[c])#insert input signal list to the first column
+                item = [item[i] for i in (0,1,3)] #1 is Voltage, 3 is Current. Delete or comment out this line to save all data. 
+                item.insert(0,timeList[c])#insert time to the first column
+                item.insert(2,PointsList[c])#insert input signal list to the third column
                 c+=1
                 rawDataList.append(','.join([str(j) for j in item])) 
             for stritem in rawDataList:
                  file_handler.write("{}\n".format(stritem))#write the string
-        print("Data file ",subdir," is saved")
+        print("Data file ",datafileout," is saved")
+        print("Output Columns from 0 to 4th respectively are:\nTime[s]\t Sampling Time[s]\t Input Signal\t Measur. V[V]\t Measur. I[A]")
         print("END")
     
     #Process acquired_points list to plot
     def draw(self):
-        timeList = [x[0] for x in dtaqsink.acquired_points]
         voltsList = [x[1] for x in dtaqsink.acquired_points]
         currentList = [x[3] for x in dtaqsink.acquired_points]         
-        timeList = [float(i) for i in timeList]
-        voltsList = [float(j) for j in voltsList]
-        currentList = [float(j) for j in currentList]
         self.graphicsView.plot(timeList,PointsList,pen=(10,10,200))
         self.graphicsView_2.plot(timeList,currentList,pen=(10,10,200))
         self.graphicsView_3.plot(timeList,voltsList,pen=(10,10,200))
-        self.IndicatorLabel.setStyleSheet("QLabel {background-color: rgb(50,200,50);border: 1.5px inset gray;border-radius: 8px;}")
     
     #Clear plots
     def clear(self):
